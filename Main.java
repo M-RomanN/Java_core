f1ac9e76786fdcd97c8073d297e2f49bcc806fef
M-RomanN <81717441+M-RomanN@users.noreply.github.com>
2023-04-19 19:24:42 +0300

    HW5
    HomeWork #5

---
+++ Main.java
@@ -0,0 +1,16 @@
+package hw_lesson_5;
+
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        AppData appData = new AppData();
+        appData.load("hw5-1.txt");
+
+        System.out.println(Arrays.toString(appData.getHeader()));
+        System.out.println(Arrays.deepToString(appData.getData()));
+
+        appData.save("hw5-1.txt");
+    }
+}
